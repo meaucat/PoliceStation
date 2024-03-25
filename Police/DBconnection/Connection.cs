ceddf4e02fd50074aeb847d7c183ee85751b4635
sm3tanus <smetanus3@gmail.com>
2024-03-25 14:50:31 +0300

    авторизация готова

---
+++ Police/DBconnection/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Police.DBconnection
+{
+    public class Connection
+    {
+        public static PoliceStationWervEntities PoliceStation = new PoliceStationWervEntities();
+    }
+}
